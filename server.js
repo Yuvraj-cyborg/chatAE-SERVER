c873a57e2a23876f2fe5a721f3f5d409981d0f5c
Yuvraj Biswal <yuvrajbiswalofficial@gamil.com>
2024-10-10 11:14:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const { createServer } = require('http');
+const server = createServer(app);
+const { Server } = require('socket.io');
+
+const port = 3001;
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Ensure this matches your client-side URL
+    methods: ["GET", "POST"],
+  }
+});
+
+io.on('connection', socket => {
+  console.log(`a user connected: ${socket.id}`);
+
+  socket.on("join_room", ({ user, room }) => {
+    socket.join(room);
+    console.log(`${user} joined room ${room}`);
+  });
+
+  socket.on("send_msg", (data) => {
+    const { room, user, message } = data;
+    const msgData = { user, message };
+    socket.to(room).emit("receive_msg", msgData);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`user disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
